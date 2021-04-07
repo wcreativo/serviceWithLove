39f442ca8c6ec9347f38da36253d77dba42bb27d
Wilbertson <w.kreativo@gmail.com>
2021-04-06 23:50:58 -0500

    Slider Home Fix

--- serviceWithLove/static/js/slider-home.js
+++ serviceWithLove/static/js/slider-home.js
@@ -1,47 +1,48 @@
-// var cont = 0;
-//
-// function loopSlider() {
-//     var xx = setInterval(function () {
-//         switch (cont) {
-//             case 0: {
-//                 $("#slider-1").fadeOut(400);
-//                 $("#slider-2").delay(400).fadeIn(400);
-//                 cont = 1;
-//
-//                 break;
-//             }
-//             case 1: {
-//                 $("#slider-2").fadeOut(400);
-//                 $("#slider-1").delay(400).fadeIn(400);
-//                 cont = 0;
-//                 break;
-//             }
-//             case 2: {
-//                 $("#slider-3").fadeOut(600);
-//                 $("#slider-4").delay(600).fadeIn(600);
-//                 cont = 1;
-//                 break;
-//             }
-//             case 3: {
-//                 $("#slider-4").fadeOut(600);
-//                 $("#slider-3").delay(600).fadeIn(600);
-//                 cont = 0;
-//                 break;
-//             }
-//         }
-//     }, 8000);
-//
-// }
-//
-// function reinitLoop(time) {
-//     clearInterval(xx);
-//     setTimeout(loopSlider(), time);
-// }
-//
-// $(window).ready(function () {
-//     $("#slider-2").hide();
-//     $("#slider-4").hide();
-//
-//     loopSlider();
-//
-// });
+var cont = 0;
+
+function loopSlider() {
+    var xx = setInterval(function () {
+        switch (cont) {
+            case 0: {
+                $("#slider-1").fadeOut(400);
+                $("#slider-2").delay(400).fadeIn(400);
+                cont = 1;
+
+                break;
+            }
+            case 1: {
+                $("#slider-2").fadeOut(400);
+                $("#slider-1").delay(400).fadeIn(400);
+                cont = 0;
+                break;
+            }
+            case 2: {
+                $("#slider-3").fadeOut(600);
+                $("#slider-4").delay(600).fadeIn(600);
+                cont = 1;
+                break;
+            }
+            case 3: {
+                $("#slider-4").fadeOut(600);
+                $("#slider-3").delay(600).fadeIn(600);
+                cont = 0;
+                break;
+            }
+        }
+    }, 8000);
+
+}
+
+function reinitLoop(time) {
+    clearInterval(xx);
+    setTimeout(loopSlider(), time);
+}
+
+$(window).ready(function () {
+    $("#slider-2").hide();
+    $("#slider-4").hide();
+    $("#slider-6").hide();
+
+    loopSlider();
+
+});
