4b82b1c75d91b7f8d15be673949f672f749ab6c7
Wilbertson <w.kreativo@gmail.com>
2021-04-09 10:30:15 -0500

    Tuning form booking

--- booking/forms.py
+++ booking/forms.py
@@ -2,22 +2,32 @@ from django import forms
 from .models import Appointment, ExtraOption
 from geoinfo.models import City, State, Country
 
+input_styles = 'rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white' \
+               ' text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2' \
+               ' focus:ring-purple-600 focus:border-transparent my-2'
+
+select_styles = 'block w-52 text-gray-700 py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm' \
+                'focus:outline-none focus:ring-primary-500 focus:border-primary-500 my-2'
+
+textarea_styles = 'flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700' \
+                  'placeholder-gray-400 rounded-lg text-base focus:outline-none focus:ring-2 ' \
+                  'focus:ring-purple-600 focus:border-transparent'
+
 
 class AppointmentForm(forms.ModelForm):
-
     class Meta:
         model = Appointment
 
         fields = [
+            'firstname',
+            'lastname',
+            'email',
+            'phone',
+            'address',
+            'suite',
             'country',
             'state',
             'city',
-            'firstname',
-            'lastname',
-            'phone',
-            'email',
-            'address',
-            'suite',
             'zipcode',
             'date',
             'time',
@@ -41,46 +51,52 @@ class AppointmentForm(forms.ModelForm):
             'total'
         ]
 
-        HOUR_CHOICES = [( (x+1)/2, f'{(x+1)/2} hours' ) for x in range(3, 24)]
+        HOUR_CHOICES = [((x + 1) / 2, f'{(x + 1) / 2} hours') for x in range(3, 24)]
         MAID_CHOICES = [(x, f'{x} maids ' if x > 1 else f'{x} maid') for x in range(1, 6)]
-        
+
         widgets = {
-           'country': forms.Select(),
-           'state': forms.Select(),
-           'city': forms.Select(),
-           'address': forms.TextInput(attrs={'placeholder': 'address where the service is needed'}),
-           'email': forms.EmailInput(attrs={'placeholder': 'contact email'}),
-           'suite': forms.TextInput(attrs={'placeholder': 'apt/suite'}),
-           'zipcode': forms.TextInput(attrs={'placeholder': 'zipcode'}),
-           'area': forms.Select(),
-           'cleaning_type': forms.Select(),
-           'room_type': forms.Select(),
-           'bathroom': forms.Select(),
-           'frequency': forms.Select(),
-           'extra_opts': forms.CheckboxSelectMultiple(),
-           'flexibility': forms.CheckboxSelectMultiple(),
-           'entry': forms.Select(),
-           'pets': forms.Select(),
-           'covid': forms.Select(),
-           'comments': forms.Textarea(),
-           'maid_qty': forms.Select(choices=MAID_CHOICES, attrs={'style': 'display: none'}),
-           'hours_qty': forms.Select(choices=HOUR_CHOICES, attrs={'style': 'display: none'}),
-           'firstname': forms.TextInput(attrs={'placeholder': 'First Name'}),
-           'lastname': forms.TextInput(attrs={'placeholder': 'Last Name'}),
-           'phone': forms.TextInput(attrs={'placeholder': 'Phone Number'}),
-           'date': forms.DateInput(format='%d/%m/%Y'),
-           'time': forms.TimeInput(format='HH:MM'),
-           'subtotal': forms.HiddenInput(),
-           'discount': forms.HiddenInput(),
-           'tax': forms.HiddenInput(),
-           'total': forms.HiddenInput()
+            'country': forms.Select(attrs={'class': select_styles}),
+            'state': forms.Select(attrs={'class': select_styles}),
+            'city': forms.Select(attrs={'class': select_styles}),
+            'address': forms.TextInput(
+                attrs={'placeholder': 'Address where the service is needed', 'class': input_styles}),
+            'email': forms.EmailInput(attrs={'placeholder': 'contact email',
+                                             'class': input_styles}),
+            'suite': forms.TextInput(attrs={'placeholder': 'Apt/Suite', 'class': input_styles}),
+            'zipcode': forms.TextInput(attrs={'placeholder': 'zipcode', 'class': input_styles}),
+            'area': forms.Select(attrs={'class': select_styles}),
+            'cleaning_type': forms.Select(attrs={'class': select_styles}),
+            'room_type': forms.Select(attrs={'class': select_styles}),
+            'bathroom': forms.Select(attrs={'class': select_styles}),
+            'frequency': forms.Select(attrs={'class': select_styles}),
+            'extra_opts': forms.CheckboxSelectMultiple(),
+            'flexibility': forms.CheckboxSelectMultiple(),
+            'entry': forms.Select(attrs={'class': select_styles}),
+            'pets': forms.Select(attrs={'class': select_styles}),
+            'covid': forms.Select(attrs={'class': select_styles}),
+            'comments': forms.Textarea(attrs={'class': textarea_styles}),
+            'maid_qty': forms.Select(choices=MAID_CHOICES, attrs={'style': 'display: none', 'class': select_styles}),
+            'hours_qty': forms.Select(choices=HOUR_CHOICES, attrs={'style': 'display: none'}),
+            'firstname': forms.TextInput(attrs={'placeholder': 'First Name',
+                                                'class': input_styles}),
+            'lastname': forms.TextInput(attrs={'placeholder': 'Last Name',
+                                               'class': input_styles}),
+            'phone': forms.TextInput(attrs={'placeholder': 'Phone Number',
+                                            'class': input_styles}),
+            'date': forms.DateInput(format='%d/%m/%Y',
+                                    attrs={'placeholder': 'Click to Choose a Date',
+                                           'class': input_styles}),
+            'time': forms.TimeInput(format='HH:MM', attrs={'placeholder': '--:--',
+                                                           'class': input_styles}),
+            'subtotal': forms.HiddenInput(),
+            'discount': forms.HiddenInput(),
+            'tax': forms.HiddenInput(),
+            'total': forms.HiddenInput()
         }
 
-        
         labels = {field: '' for field in fields}
 
     def __init__(self, *args, **kwargs):
-        
         super().__init__(*args, **kwargs)
         self.fields['area'].empty_label = None
         self.fields['cleaning_type'].empty_label = None
