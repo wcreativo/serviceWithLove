cfd60031a6f91892b08d449d20f481409cfcd8ad
SoundWave <detalus@gmail.com>
2021-04-12 07:00:48 -0500

    threaded email process done

--- common/mailman.py
+++ common/mailman.py
@@ -1,29 +1,39 @@
+import asyncio
 import os
+import threading
 
+from booking.models import Appointment
 from django.core import mail
 from django.template.loader import render_to_string
-from booking.models import Appointment
+from threading import Thread
 
 
-def sendingmail(instance: Appointment) -> bool:
+class EmailThread(threading.Thread):
+    def __init__(self, instance):
+        self.instance = instance
+        threading.Thread.__init__(self)
 
-    context = {
-        'appointment': instance,
-    }
+    def run (self):
+        context = {
+            'appointment': self.instance,
+        }
 
-    html = render_to_string('email/service_receipt.html', context=context)
-    txt = render_to_string('email/service_receipt.txt', context=context)
+        html = render_to_string('email/service_receipt.html', context=context)
+        txt = render_to_string('email/service_receipt.txt', context=context)
+        try:
+            mail.send_mail(
+                subject='Clean Service Appointment Info',
+                message=txt,
+                html_message=html,
+                from_email=os.environ.get('MAIL_USER'),
+                recipient_list=[self.instance.email,os.environ.get('MAIL_ADMIN')],
+                fail_silently=False
+            )
+            print(f'[INFO] mail successfully sended to {self.instance.email} service order # {self.instance.id}')
+        except Exception as e:
+            print(f'[ERROR] error sending mail to {self.instance.email} service order {self.instance.id}: {e}')
+             
 
-    try:
-        mail.send_mail(
-            subject='Clean Service Appointment Info',
-            message=txt,
-            html_message=html,
-            from_email=os.environ.get('MAIL_USER'),
-            recipient_list=[instance.email,os.environ.get('MAIL_ADMIN')],
-            fail_silently=False
-        )
-        return True
-    except Exception as e:
-        print(f'[ERROR] error sending mail: {e}')
-        return False
+def send_html_mail(instance: Appointment) -> bool :
+    EmailThread(instance).start()
+    return True
